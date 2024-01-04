577f708a14fcb640387488eed986d0b3ae1b1333
kwamecody <kwamecody@gmail.com>
2024-01-04 19:01:34 +0000

    configured profile route

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,17 @@
+const express = require('express')
+const User = require("../models/User");
+
+
 module.exports = {
   getIndex: (req, res) => {
     res.render("index.ejs");
   },
+  getProfile: async (req, res) => {
+    try {
+      const user = await User.find({ user: req.user.id });
+      res.render("profile.ejs", { user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 };
