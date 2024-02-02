bce9431b9e55408c8c63f6e92dcaf27678c1ccad
Kwame Cody <kwamecody@gmail.com>
2024-02-02 05:54:13 +0000

    fetched currency data

--- controllers/home.js
+++ controllers/home.js
@@ -8,10 +8,40 @@ module.exports = {
   },
   getProfile: async (req, res) => {
     try {
-      const user = await User.find({ user: req.user.id });
-      res.render("profile.ejs", { user: req.user });
+      const currencies = await module.exports.getCurrencies(req, res);
+      res.render("profile.ejs", {currencies });
     } catch (err) {
       console.log(err);
+    }
+  },
+  getCurrencies: async (req, res) => {
+        try {
+          const currencyResponse = await fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json');
+          const currencyData = await currencyResponse.json();
+
+          const currencies = Object.keys(currencyData);
+
+          return currencies
+    } catch (err) {
+      console.error(err);
+    }
+    
+  },
+  convertCurrency : async (req,res) => {
+    try {
+      const { amount, fromCurrency, toCurrency } = req.body;
+      
+      // Fetch exchange rates for conversion
+      const exchangeRateResponse = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/${fromCurrency}-${toCurrency}`);
+      const exchangeRateData = await exchangeRateResponse.json();
+      
+      // Calculate converted amount
+      const convertedAmount = amount * exchangeRateData[toCurrency];
+
+      // Render the result
+      console.log(convertedAmount)
+    } catch (err) {
+      console.error(err);
     }
   }
 };
